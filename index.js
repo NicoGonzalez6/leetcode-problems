eb9183f9fe8a21c55a367c7af1965e91431d7989
NicoGonzalez <NICOLAS.GONZALEZ@usan.com>
2024-06-02 01:18:19 -0300

    updates

---
+++ index.js
@@ -0,0 +1,14 @@
+const person1 = {
+  name: "nico",
+  lastName: "gonzalez",
+};
+
+const person2 = {
+  name: "pedro",
+  lastName: "gomez",
+};
+
+const nico = "1";
+const pedro = 1;
+
+console.log(a === "1");
